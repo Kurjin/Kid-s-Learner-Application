cee1491b342a4f57605698a8ca8af531ccb90b92
Main <euginemanliclic16@gmail.com>
2026-02-18 22:19:59 +0800

    register function, refactoring userRepo and model queries

--- model/user.vb
+++ model/user.vb
@@ -1,8 +1,22 @@
 ﻿Public Class user
     'Represent user data : Registration, Authentication, Profile
     Public Property Id As Integer
-    Public Property Username As String
-    Public Property PasswordHash As String
+    Public Property username As String
+    Public Property password As String
     Public Property Salt As String
     Public Property CreatedAt As DateTime
+    Public Property PasswordHash As String
+
+    ' Call this method from a Register button or other flow
+    Public Sub RegisterUserPrompt(username As String, password As String)
+        If String.IsNullOrWhiteSpace(username) Then Return
+        'call user repo to register, handle success/failure
+        Dim repo As New sqlUserRepo()
+        If repo.Register(username, password) Then
+            MessageBox.Show("Account created")
+        Else
+            MessageBox.Show("Registration failed (username may exist or DB error)")
+        End If
+    End Sub
 End Class
+
